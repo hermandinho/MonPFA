3b1dab89e250c9edafb0bd03d06ef7220c73eb08
Herman Demsong <herman.tsamo@satoripop.com>
2016-05-10 10:56:02 +0200

    added somme JS files

---
+++ src/PFA/MaillingBundle/Resources/public/MaillingComponent.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Herman on 10/05/2016.
+ */
+
+
+var MaaillingComponent = (function () {
+    var FolderList = React.createClass({
+        displayName: 'FolderList',
+        displayClass: "MaaillingComponent",
+        getInitialState: function () {
+            return {
+                folders: []
+            };
+        },
+        
+        render: function () {
+            
+        }
+
+    });
+})();
+
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        cache: false,
+        url: "/",
+        dataType: "json",
+        success: function (data) {
+            //console.log( data);
+            var source = (data);
+            //console.log(source);
+            ReactDOM.render(React.createElement(MaaillingComponent, {interactions: source}), document.getElementById("timeLine"));
+        },
+        error: function (e, err) {
+
+        }
+    });
+});
